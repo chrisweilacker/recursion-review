f1c5bdf40b70dae3b12d0671c27623431dd627d0
Sadie Gill <thesadie@gmail.com>
2018-09-30 11:19:20 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    var stringArray = '';
+    for (var i = 0; i < obj.length; i ++) {
+      stringArray += stringifyJSON(obj[i]) + ',';
+    }
+    return '[' + stringArray.slice(0, stringArray.length - 1) + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var stringObj = '';
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        stringObj += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + stringObj.slice(0, stringObj.length - 1) + '}';
+  }
+
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else {
+    return '' + obj;
+  }
 };
